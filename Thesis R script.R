4ec7a4387c7a2c64a97523c4f6bcd8e078c60427
ambervdmeijs <ambervdmeijs@hotmail.com>
2017-08-01 16:09:45 +0200

    Update Thesis R script.R

--- Thesis R script.R
+++ Thesis R script.R
@@ -11,7 +11,3 @@ library("utils")
 ipumsdeel1 <- read_excel("D:/Amber/Documenten/School/Tilburg University/Master/Thesis/IPUMS2001 deel 1.xlsx")
 
 # Creating missing values -----------------------------------------------------------------------------------
-prop.m = 0.05
-mcar   = runif(ni*nj, min=0, max=1)
-y.mcar = ifelse(mcar<prop.m, NA, y)  
-View(ipumsdeel1)
